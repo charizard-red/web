cb8e4aed0dd1ef5d8c337bf7a6fa94e5bddd7768
Herdanu Yudistira Putra <heh.danu@gmail.com>
2018-05-01 18:48:41 +0700

    adding content

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
-import Header from './components/Header'
+import Header from './components/Header';
+import Example from './components/FirstPC';
 
 class App extends Component {
   render() {
     return (
       <div>
         <Header />
+        <Example />
     </div>
     );
   }
