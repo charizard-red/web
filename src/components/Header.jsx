4fac8387ec7ad078bc69e5fec86ca101ab49da15
Herdanu Yudistira Putra <heh.danu@gmail.com>
2018-05-01 15:30:03 +0700

    navbar and search bar

---
+++ src/components/Header.jsx
@@ -0,0 +1,54 @@
+import React, { Component } from 'react';
+import {
+  Collapse,
+  Navbar,
+  NavbarToggler,
+  NavbarBrand,
+  Nav,
+  NavItem,
+  NavLink,
+  UncontrolledDropdown,
+  DropdownToggle,
+  DropdownMenu,
+  DropdownItem } from 'reactstrap';
+
+class Header extends Component {
+  constructor(props) {
+    super(props);
+
+    this.toggle = this.toggle.bind(this);
+    this.state = {
+      isOpen: false
+    };
+  }
+  toggle() {
+    this.setState({
+      isOpen: !this.state.isOpen
+    });
+  }
+  render() {
+    return (
+      <div>
+        <Navbar color="light" light expand="md">
+          <NavbarBrand href="/">OTW-DOC</NavbarBrand>
+          <NavbarToggler onClick={this.toggle} />
+          <Collapse isOpen={this.state.isOpen} navbar>
+            <Nav className="ml-auto" navbar>
+              <NavItem>
+                <NavLink href="">HOME</NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink href="">LOG IN</NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink href="">SIGN UP</NavLink>
+              </NavItem>
+            </Nav>
+          </Collapse>
+        </Navbar>
+      </div>
+    );
+  }
+}
+
+export default Header;
---
+++ src/components/Search.jsx
@@ -0,0 +1,42 @@
+import React, { Component } from 'react'
+
+class SearchBar extends Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      hospital: ''
+    }
+  }
+
+  handleSearch (e) {
+    this.setState({ hospital: e.target.value })
+  }
+
+  handleGoClick () {
+    if (!this.props.github.isFetchingUser) {
+      this.props.actions.fetchUser(this.state)
+    }
+  }
+
+  render () {
+    return (
+      <div className='searchbar-container'>
+        <form onSubmit={e => e.preventDefault()}>
+          <input
+            type='text'
+            size='45'
+            placeholder='search hospital nearby'
+            onChange={this.handleSearch.bind(this)}
+            value={this.state.hospital} />
+          <button
+            type='submit'
+            onClick={this.handleGoClick.bind(this)}>
+            Search
+          </button>
+        </form>
+      </div>
+    )
+  }
+}
+
+export default SearchBar;
