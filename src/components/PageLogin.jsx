a0c3c255b8e6cf6a91b53b3c678130cb17101776
Kien Doan <kien.doan@gmail.com>
2018-05-04 15:52:31 +0700

    Sign Up sheet first version

--- src/components/PageLogin.jsx
+++ src/components/PageLogin.jsx
@@ -1,14 +1,35 @@
-import React, { Component } from 'react';
-import PageControl from './PageControl'
+import React, { Component } from "react";
+import PageControl from "./PageControl";
+import { Button, Form, FormGroup, Label, Input, FormText } from "reactstrap";
 
-class PageLogin extends Component{
-  render(){
-    return(
+class PageLogin extends Component {
+  render() {
+    return (
       <PageControl>
         <h1>Login Page</h1>
-      </PageControl>
 
-    )
+        <FormGroup>
+          <Label for="exampleEmail">Email</Label>
+          <Input
+            type="email"
+            name="email"
+            id="exampleEmail"
+            placeholder="E-mail"
+          />
+        </FormGroup>
+        <FormGroup>
+          <Label for="examplePassword">Password</Label>
+          <Input
+            type="password"
+            name="password"
+            id="examplePassword"
+            placeholder="Password"
+          />
+        </FormGroup>
+
+        <Button>Submit</Button>
+      </PageControl>
+    );
   }
 }
 export default PageLogin;
