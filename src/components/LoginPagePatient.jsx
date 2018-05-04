9a31ee553ccbb101745bd8e9d5175f7e45e40580
Muhammad Najib K <virtualnajib@gmail.com>
2018-05-04 16:15:46 +0700

    Adding form patient data

--- src/components/LoginPagePatient.jsx
+++ src/components/LoginPagePatient.jsx
@@ -15,12 +15,10 @@ class LoginPagePatient extends Component {
       <PageControl>
         <form>
           <FormGroup name="patientData">
-            <Label>Full Name</Label>
-            <Input placeholder="Type your name"/>
-            <Label>Age</Label>
-            <Input placeholder="How old are you?"/>
-            <Label>Birthdate</Label>
-            <Input type="date" name="date" id="exampleDate" placeholder="date placeholder"/>
+            <Label>Username</Label>
+            <Input placeholder=""/>
+            <Label>Email</Label>
+            <Input placeholder=""/>
             <label>Gender</label>
             <FormGroup check="check">
               <Label check="check">
@@ -34,6 +32,40 @@ class LoginPagePatient extends Component {
                 Female
               </Label>
             </FormGroup>
+            <Label>Age</Label>
+            <Input placeholder=""/>
+            <Label>Phone</Label>
+            <Input placeholder=""/>
+            <Label>Address</Label>
+            <Input type="textarea" name="text" id="exampleText" />
+            <Label>Birthdate</Label>
+            <Input type="date" name="date" id="exampleDate" placeholder="date placeholder"/>
+            <Label>Select a Clinic</Label>
+            <Input type="select" name="select" id="exampleSelect">
+              <option>1</option>
+              <option>2</option>
+              <option>3</option>
+              <option>4</option>
+              <option>5</option>
+            </Input>
+            <Label>Select a Specialty</Label>
+            <Input type="select" name="select" id="exampleSelect">
+              <option>1</option>
+              <option>2</option>
+              <option>3</option>
+              <option>4</option>
+              <option>5</option>
+            </Input>
+            <label>Appointment Date & Time</label>
+            <Input type="date" name="date" id="exampleDate" placeholder="date placeholder"/>
+            <Input type="select" name="select" id="exampleSelect">
+              <option>1</option>
+              <option>2</option>
+              <option>3</option>
+              <option>4</option>
+              <option>5</option>
+            </Input>
+
           </FormGroup>
           <Label>Address</Label>
           <Input type="textarea" name="text" id="exampleText" />
--- src/components/PageLogin.jsx
+++ src/components/PageLogin.jsx
@@ -8,9 +8,15 @@ class PageLogin extends Component {
         <form name='login'>
           <div class="wrapper">
             <form class="form-signin">
-              <h2 class="form-signin-heading">Doctor Login</h2>
-
+              <h2 class="form-signin-heading">User/Patient Login</h2>
               <button className='btn btn-lg btn-danger btn-block fa fa-google' type="submit">Login via Google</button>
+            </form>
+            <form class="form-signin">
+              <h2 class="form-signin-heading">Doctor Login</h2>
+              <input type="text" placeholder="username" />
+              <input type="text" placeholder="password" />
+              <button className='btn btn-lg btn-danger btn-block' type="submit">Login</button>
+              <button className='btn btn-lg btn-danger btn-block' type="submit">SignUp</button>
             </form>
           </div>
         </form>
